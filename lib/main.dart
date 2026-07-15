a4f6b3b05c94b56983702d0183fb62931c95e5a1
denn <thunnymaridenn@gmail.com>
2026-07-15 13:49:11 +0700

    initial commit of scammie

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import './ui/screens/welcomeScreen.dart';
+
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Welcomescreen(),));
+}
