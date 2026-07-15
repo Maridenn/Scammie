ab9ed3812ff5e4e0ec898b3ca13d2b03e3006e87
denn <thunnymaridenn@gmail.com>
2026-07-15 14:09:50 +0700

    welcome screen strings

---
+++ lib/ui/utils/welcomePage_strings.dart
@@ -0,0 +1,16 @@
+class WelcomepageStrings {
+  static const image1 = "../../assets/images/logo.jpg";
+  static const title1 = "Scammie Simulator";
+  static const subTitle1 =
+      "A simulator to test your likelihood to get cyber scammed and to raise awareness of it";
+
+  static const image2 = "../../assets/images/messagePhoto.jpg";
+  static const title2 = "Immersive environment";
+  static const subTitle2 =
+      "Made to look like real sms and message app environents to make users experience to like they are texting";
+
+  static const image3 = "../../assets/images/hackerPhoto.jpg";
+  static const title3 = "Real scamming texts";
+  static const subTitle3 =
+      "Scammer behaviorals including social engineering and sweets talks to make the simulator feels real";
+}
