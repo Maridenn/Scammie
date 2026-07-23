009faadf075f09db95658ed116081b8efc2d9170
denn <thunnymaridenn@gmail.com>
2026-07-23 23:14:37 +0700

    add: authgate to determine if it is already login then will stay in dahboard if not go to welcome

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import './data/repositories/auth_repository.dart';
 import './ui/screens/welcome_screen.dart';
+import './ui/screens/dashboard.dart';
 import './ui/theme/app_theme.dart';
 
 void main() async {
@@ -20,7 +23,28 @@ class ScammieApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: Welcomescreen(),
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: AuthRepository().authState,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        return snapshot.hasData
+            ? const DashboardScreen()
+            : const Welcomescreen();
+      },
     );
   }
 }
