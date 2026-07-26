c23041f79ed0db9185d7dec75256c17a9744b04c
denn <thunnymaridenn@gmail.com>
2026-07-27 00:21:57 +0700

    change home

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'chat_screen.dart';
+import 'difficulty_screen.dart';
+import '../../data/repositories/scenario_repository.dart';
 import '../../models/dashboard_items.dart';
 import '../theme/app_theme.dart';
 
-// a placeholder before the database
-List<DashboardItem> _scenarios = [
+List<DashboardItem> scenarios = [
   DashboardItem(
+    scenarioId: "it_otp",
     image: "assets/images/hackerPhoto.jpg",
     heading: "Message with IT",
     subHeading: "IT Support and Phishing",
   ),
   DashboardItem(
+    scenarioId: "delivery",
     image: "assets/images/messagePhoto.jpg",
     heading: "Failed Delivery",
     subHeading: "Dilivery scam",
   ),
   DashboardItem(
+    scenarioId: "job",
     image: "assets/images/login.jpg",
     heading: "Easy Job Offer",
     subHeading: "Telegram Recruiter and Job Scam",
@@ -23,20 +26,20 @@ List<DashboardItem> _scenarios = [
 ];
 
 class ScenarioList extends StatelessWidget {
-  const ScenarioList();
+  const ScenarioList({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       padding: const EdgeInsets.fromLTRB(24, 24, 24, 24),
-      itemCount: _scenarios.length,
-      itemBuilder: (context, index) => _ScenarioCard(item: _scenarios[index]),
+      itemCount: scenarios.length,
+      itemBuilder: (context, index) => ScenarioCard(item: scenarios[index]),
     );
   }
 }
 
-class _ScenarioCard extends StatelessWidget {
-  const _ScenarioCard({required this.item});
+class ScenarioCard extends StatelessWidget {
+  const ScenarioCard({super.key, required this.item});
 
   final DashboardItem item;
 
@@ -49,10 +52,16 @@ class _ScenarioCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         child: InkWell(
           borderRadius: BorderRadius.circular(16),
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => const ChatScreen()),
-          ),
+          onTap: () {
+            final scenario = ScenarioRepository().getById(item.scenarioId);
+            if (scenario == null) return;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => DifficultyScreen(scenario: scenario),
+              ),
+            );
+          },
           child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(16),
