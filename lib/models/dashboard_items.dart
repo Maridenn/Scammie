8550835fab3bb2f1de6622faa5db6c58d9347e17
mengyeanuch <mengyeanuch168168@gmail.com>
2026-07-26 23:34:09 +0700

    change dashboard items

--- lib/models/dashboard_items.dart
+++ lib/models/dashboard_items.dart
@@ -2,10 +2,12 @@ class DashboardItem {
   final String image;
   final String heading;
   final String subHeading;
+  final String scenarioId;
 
   DashboardItem({
     required this.image,
     required this.heading,
     required this.subHeading,
+    required this.scenarioId,
   });
 }
