929e0f66ce35571ee44a854eaa0656a6f7c727e0
mengyeanuch <mengyeanuch168168@gmail.com>
2026-07-26 23:36:36 +0700

    change scenario repository

--- lib/data/repositories/scenario_repository.dart
+++ lib/data/repositories/scenario_repository.dart
@@ -1,11 +1,17 @@
 import '../../models/scenario.dart';
-import '../local/default_scenarios.dart';
+import '../scenarios/failed_delivery.dart';
+import '../scenarios/message_with_it.dart';
+import '../scenarios/telegram_job.dart';
 
 class ScenarioRepository {
-  List<Scenario> getAll() => defaultScenarios;
+  List<Scenario> getAll() => const [
+    messageWithIt,
+    failedDelivery,
+    telegramJob,
+  ];
 
   Scenario? getById(String id) {
-    for (final s in defaultScenarios) {
+    for (final s in getAll()) {
       if (s.id == id) return s;
     }
     return null;
